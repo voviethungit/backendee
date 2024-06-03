9187504a76aa99e672bf45c21a9f23aaedf3da85
Vo Viet Hung Developer <90433491+voviethungit@users.noreply.github.com>
2024-06-03 16:26:50 +0700

    Update Database.js

--- backend/db/Database.js
+++ backend/db/Database.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectDatabase = () => {
-  mongoose
-    .connect(process.env.DB_URL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then((data) => {
-      console.log(`mongod connected with server: ${data.connection.host}`);
-    });
+  mongoose.connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then((data) => {
+    console.log(`Mongodb connected with server: ${data.connection.host}`);
+  }).catch((error) => {
+    console.log(`Error connecting to MongoDB: ${error.message}`);
+  });
 };
 
 module.exports = connectDatabase;
